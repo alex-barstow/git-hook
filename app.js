5b7074e14623d5f65160e138749eb572d0148000
Alex Barstow <alexander.barstow@gmail.com>
2017-05-01 22:46:17 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const exec = require('child_process').exec;
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+	console.log('GET /');
+});
+
+app.get('/payload', (req, res) => {
+  res.sendStatus(200);
+	console.log('get /payload');
+});
+
+app.post('/payload', (req, res) {
+  res.sendStatus(200);
+  console.log(res);
+});
+
+app.listen(5000, () => {
+	console.log('listening on port 5000...')
+});
